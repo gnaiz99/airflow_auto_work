9a4fad2064bdcefa3007bec5f568d4b7203a6604
giangntg <giangntg2301@gmail.com>
2025-04-01 00:04:17 +0700

    Đã tự động gửi được mail cho list mail, nhưng chưa chia được theo role

--- demo_test_func/test_connect_ggsheet.py
+++ demo_test_func/test_connect_ggsheet.py
@@ -7,8 +7,8 @@ from email.mime.multipart import MIMEMultipart
 
 #1. Kết nối API - đọc file ggsheet
 # Đọc file API kết nối ggsheet
-# gs = gspread.service_account(r'C:\Users\Admin\Downloads\test-connect-ggsheet-454216-7e9f4c02bdda.json') #Địa chỉ file test-connect-ggsheet trong máy local
-gs = gspread.service_account(r"C:\Users\giang.nt\Downloads\file hoàn thành\Py\test-connect-ggsheet-454216-7e9f4c02bdda.json") #Địa chỉ file trong máy cty
+gs = gspread.service_account(r'C:\Users\Admin\Downloads\test-connect-ggsheet-454216-7e9f4c02bdda.json') #Địa chỉ file test-connect-ggsheet trong máy local
+# gs = gspread.service_account(r"C:\Users\giang.nt\Downloads\file hoàn thành\Py\test-connect-ggsheet-454216-7e9f4c02bdda.json") #Địa chỉ file trong máy cty
 #Kết nối bằng key trong url
 list_mail = gs.open_by_key('19AtCkKfcXNQp8d1hjPEK6wfGZhJv6cKxLI6YzP7cyQc').sheet1 #Lấy sheet đầu tiên
 list_mail = list_mail.get_all_records()
@@ -29,7 +29,9 @@ df_info = df_list_mail.merge(df_role, on="id", how="left")
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
 EMAIL_SENDER = "giangntg2301@gmail.com"
-EMAIL_PASSWORD = "nguyentruonggiang"
+#Phải dùng mật khẩu ứng dụng để đăng nhập vào mail
+#https://myaccount.google.com/apppasswords link lấy mk ứng dụng
+EMAIL_PASSWORD = "hotb aynh lagg crzo"
 
 # Hàm gửi email
 def send_email(to_email, subject, body):
@@ -50,5 +52,23 @@ def send_email(to_email, subject, body):
         print(f"❌ Failed to send email to {to_email}: {e}")
 
 
+# Hàm gửi email cho danh sách lấy từ Google Sheets
+def send_bulk_emails():
+    email_data = df_info["email"].tolist()
+    rlevel = df_info["level"].tolist()
 
+    # Xem lại logic gửi mail, đang gửi mail cho tất cả trạng thái của tất cả mail
+    for row in email_data:
+        email = row
+        for rl in rlevel:
+            level = rl
 
+            if email and level:
+                subject = "Thông báo kết quả"
+                body = "Thành công" if level == 1 else "Thất bại" if level == 2 else None
+
+                if body:
+                    send_email(email, subject, body)
+
+# Chạy hàm gửi email
+send_bulk_emails()

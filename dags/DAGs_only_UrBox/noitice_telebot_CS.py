948a386c68056173390a7a0433db075be187194d
giang <giang.nt@urbox.vn>
2025-05-17 23:30:32 +0700

    update DAGs DE review, vẫn chưa chạy được

--- dags/DAGs_only_UrBox/noitice_telebot_CS.py
+++ dags/DAGs_only_UrBox/noitice_telebot_CS.py
@@ -6,7 +6,7 @@ import requests
 from airflow.providers.telegram.hooks.telegram import TelegramHook
 from sqlalchemy.sql.functions import current_date
 
-#TELEGRAM_TOKEN = '8021130212:AAHx7u70ptnb4VpZjDrsMu5i-yYY-dlBzyA' # tuyệt đối ko ghi thông tin credential lên code
+TELEGRAM_TOKEN = '8021130212:AAHx7u70ptnb4VpZjDrsMu5i-yYY-dlBzyA' # tuyệt đối ko ghi thông tin credential lên code
 
 TELEGRAM_CHAT_ID = '-4841407782'
 
@@ -18,13 +18,18 @@ default_args = {
 
 # ==== Hàm gửi telegram ====
 def send_telegram_message(message: str):
-    telegram_conn_id = "telegram_conn_id"
+    # telegram_conn_id = "telegram_conn_id"
+    # telegram_hook = (
+    #         telegram_conn_id = telegram_conn_id,
+    #         chat_id=TELEGRAM_CHAT_ID,
+    # )
+    # telegram_hook.send_message(api_params={"text": message})
 
-    telegram_hook = (
-            telegram_conn_id=telegram_conn_id,
-            chat_id="-4841407782",
-        )
-    telegram_hook.send_message(api_params={"text": message})
+    hook = TelegramHook(
+        telegram_token=TELEGRAM_TOKEN,
+        chat_id=TELEGRAM_CHAT_ID
+    )
+    hook.send_message(api_params={"text": message})
 
 
 # ==== Hàm xử lý chính ====
@@ -57,7 +62,7 @@ def get_data_and_send(**kwargs):
        where 1=1
        and match(LOWER(m.content) , 'lg|urcard|urgift')
        AND m.role = 'user'
-       and toDate(m.created_at) between '{current_date.date()}' and '{previous_date.date()}'
+       and toDate(m.created_at) between '{previous_date.date()}' and '{previous_date.date() + timedelta(days=1)}'
        """
 
     # Lưu ý: Object taọ lại 2 lần là phí tài nguyên
@@ -69,7 +74,7 @@ def get_data_and_send(**kwargs):
 
     html_table = list_agent_ask_bot.to_html(index=False)
     message = f"""<b>Dear team CSKH,</b>
-                \nData gửi thông tin số lượng check bot hôm qua ngày {previous_date}: <b>{number_ask_bot}</b>
+                \nData gửi thông tin số lượng check bot ngày {previous_date}: <b>{number_ask_bot}</b>
                 \n<b>{html_table} </b>"""
     send_telegram_message(message)
 
@@ -78,7 +83,7 @@ with DAG(
     dag_id='clickhouse_to_telegram_report',
     default_args=default_args,
     start_date=datetime(2025, 5, 15),
-    schedule_interval='30 10 * * *',  # Mỗi ngày lúc 10h30 sáng
+    schedule_interval='5 23 * * *',  # Mỗi ngày lúc 10h30 sáng
     catchup=True,
     tags=['report', 'telegram', 'clickhouse']
 ) as dag:
